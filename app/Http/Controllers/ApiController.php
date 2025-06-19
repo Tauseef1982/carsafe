9248d48251426771ecf982f5a1f967f821e3d96f
Tauseef <rehman.tuseef757@gmail.com>
2025-06-19 16:50:51 +0500

    webhook for trips

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
+use App\Models\AccountPayment;
+use App\Models\BatchPayment;
+use App\Models\CreditCard;
 use App\Models\Driver;
 use App\Models\Payment;
 use App\Models\Trip;
+use App\Services\CardKnoxService;
+use App\Services\CubeContact;
+use App\Services\LogService;
+use App\Services\TokenService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ApiController extends Controller
 {
@@ -155,6 +167,98 @@ class ApiController extends Controller
         }
 
     }
+
+     public function getWebHookTrip(Request $request){
+
+
+        $tripContent = $request->getContent();
+         Log::info($tripContent);
+        $trip = json_decode( $tripContent, true);
+
+
+        // Check if 'start' is valid
+                if ($trip['start'] != '-' && $trip['start'] != '') {
+
+                    if (isset($trip['driverId'])) {
+                        if ($trip['driverId'] != '') {
+
+
+                            $dateTime = Carbon::createFromFormat('m/d/Y h:i A', $trip['start']);
+                            $date = $dateTime->format('Y-m-d'); // e.g., '2024-09-01'
+                            $time = $dateTime->format('H:i:s'); // e.g., '12:25:00'
+
+                            $existingTrip = Trip::where('trip_id', (int)$trip['id'])->first();
+
+
+                                $to_location = $trip['route.drop_off_text'];
+
+
+
+
+                            if ($existingTrip) {
+                                // Check if the payment method is cash
+                                if ($existingTrip->payment_method === 'cash') {
+                                    if ($existingTrip->temp_data == null || $existingTrip->temp_data == '') {
+                                $tsDelivered = (!empty($trip['ts.delivered']) && $trip['ts.delivered'] !== '-')
+                                    ? date("Y-m-d H:i:s", strtotime($trip['ts.delivered']))
+                                    : null;
+
+                                        $ickedup = !empty($trip['icked up']) ? date("Y-m-d H:i:s", strtotime($trip['icked up'])) : null;
+                                        $existingTrip->update([
+                                            'location_from' => $trip['route.pick_up_text'],
+                                            'location_to' => $to_location,
+                                            'date' => $date,
+                                            'time' => $time,
+                                            'trip_cost' => !empty($trip['fx.trip_base']) && $trip['fx.trip_base'] != 0 ? $trip['fx.trip_base'] : $trip['estimatedPrice'],
+                                            'driver_id' => $trip['driverId'],
+                                            'account_number' => $trip['account.name'],
+                                            'passenger_phone' => $trip['passenger.phone'].'',
+                                            'estimated_cost' => !empty($trip['fx.trip_base']) && $trip['fx.trip_base'] != 0 ? $trip['fx.trip_base'] : $trip['estimatedPrice'],
+                                            'status' => $trip['event'],
+                                            'ts_delivered' => $tsDelivered,
+                                            'icked_up' => $ickedup,
+
+                                        ]);
+                                        return response()->json('updated');
+
+                                    }
+                                }
+                            } else {
+
+                        $ickedup = !empty($trip['icked up']) ? date("Y-m-d H:i:s", strtotime($trip['icked up'])) : null;
+                        $tsDelivered = (!empty($trip['ts.delivered']) && $trip['ts.delivered'] !== '-')
+                            ? date("Y-m-d H:i:s", strtotime($trip['ts.delivered']))
+                            : null;
+
+                                Trip::create([
+                                    'trip_id' => (int)$trip['id'],
+                                    'location_from' => $trip['route.pick_up_text'],
+                                    'location_to' => $to_location,
+                                    'date' => $date,
+                                    'time' => $time,
+                                    'trip_cost' => !empty($trip['fx.grand_total']) && $trip['fx.grand_total'] != 0 ? $trip['fx.grand_total'] : $trip['estimatedPrice'],
+                                    'trip_extras' => !empty($trip['fx.trip_extras']) && $trip['fx.trip_extras'] != 0 ? $trip['fx.trip_extras'] : 0,
+                                    'driver_id' => $trip['driverId'],
+                                    'account_number' => $trip['account.name'],
+                                    'passenger_phone' => $trip['passenger.phone'],
+                                    'estimated_cost' => !empty($trip['fx.trip_base']) && $trip['fx.trip_base'] != 0 ? $trip['fx.trip_base'] : $trip['estimatedPrice'],
+                                    'status' => $trip['event'],
+                                    'ts_delivered' => $tsDelivered,
+                                    'icked_up' => $ickedup,
+
+                                ]);
+
+                              return response()->json('created');
+
+                            }
+
+
+
+                        }
+                    }
+
+                }
+            }
 
 
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/testwebhook', [TripController::class, 'cubePayment']);
+Route::post('/webhook-store-trip', [\App\Http\Controllers\ApiController::class, 'getWebHookTrip']);
 
