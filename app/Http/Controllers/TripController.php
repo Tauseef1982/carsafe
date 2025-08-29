0d8e43b499be22741c2fa951095f6f4e95ea2eaf
Tauseef <rehman.tuseef757@gmail.com>
2025-08-29 21:07:53 +0500

    wait back

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -50,20 +50,20 @@ class TripController extends Controller
                 ->where('trips.payment_method', 'cash')
                 ->where('status', 'NOT LIKE', '%Cancelled%')
                 ->where('status', 'NOT LIKE', '%Client canceled%')
-                // ->whereNotNull('icked_up')
-                // ->where('icked_up', '!=', '')
-                // ->where(function ($query) {
-                //     $query->whereNull('ts_delivered')
-                //         ->orWhereRaw("COALESCE(ts_delivered, '') = ''")
-                //         ->orWhereBetween('ts_delivered', [now()->subMinutes(15)->format('Y-m-d H:i:s'), now()->format('Y-m-d H:i:s')]);
-                // })
-                // ->whereNotExists(function ($query) use ($driverId) {
-                //     $query->select(DB::raw(1))
-                //         ->from('trips as future_trips')
-                //         ->whereColumn('future_trips.driver_id', 'trips.driver_id')
-                //         ->where('future_trips.icked_up', '>', DB::raw('trips.icked_up'));
+                ->whereNotNull('icked_up')
+                ->where('icked_up', '!=', '')
+                ->where(function ($query) {
+                    $query->whereNull('ts_delivered')
+                        ->orWhereRaw("COALESCE(ts_delivered, '') = ''")
+                        ->orWhereBetween('ts_delivered', [now()->subMinutes(15)->format('Y-m-d H:i:s'), now()->format('Y-m-d H:i:s')]);
+                })
+                ->whereNotExists(function ($query) use ($driverId) {
+                    $query->select(DB::raw(1))
+                        ->from('trips as future_trips')
+                        ->whereColumn('future_trips.driver_id', 'trips.driver_id')
+                        ->where('future_trips.icked_up', '>', DB::raw('trips.icked_up'));
 
-                // })
+                })
                 ->select('trips.*')
                 ->orderBy('trips.date', 'desc')
                 ->orderBy('trips.time', 'desc')
