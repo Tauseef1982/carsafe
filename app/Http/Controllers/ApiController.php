e7f941917911d54b6fa5cdc19dc3f0dd8cc5771f
Tauseef <rehman.tuseef757@gmail.com>
2026-09-19 23:13:07 +0500

    show account balance

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -781,10 +781,14 @@ public function getWebHookTrip(Request $request)
             DB::commit();
         } elseif ($cardknoxResponse['status'] == 'declined') {
              Log::info('got declined with this card ');
+            // return response()->json([
+            //     'valid' => false,
+            //     'message' => 'Card Decline'
+            // ]);
             return response()->json([
-                'valid' => false,
-                'message' => 'Card Decline'
-            ]);
+            'valid' => false,
+            'message' => 'Card Decline'
+        ], 400);
 
         } else {
             return response()->json([
